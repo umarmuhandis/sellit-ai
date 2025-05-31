2e046c3208561809c3dac5399f283983ffe3dc06
Michael Shimeles <michaelwasihun96@gmail.com>
2025-05-31 15:24:59 -0400

    critical bug fix

--- convex/users.ts
+++ convex/users.ts
@@ -14,7 +14,7 @@ export const findUserByToken = query({
     // Check if we've already stored this identity before
     const user = await ctx.db
       .query("users")
-      // .withIndex("by_token", (q) => q.eq("tokenIdentifier", identity.subject))
+      .withIndex("by_token", (q) => q.eq("tokenIdentifier", identity.subject))
       .unique();
 
     if (user !== null) {
@@ -36,7 +36,7 @@ export const upsertUser = mutation({
     // Check if user exists
     const existingUser = await ctx.db
       .query("users")
-      // .withIndex("by_token", (q) => q.eq("tokenIdentifier", identity.subject))
+      .withIndex("by_token", (q) => q.eq("tokenIdentifier", identity.subject))
       .unique();
 
     if (existingUser) {
