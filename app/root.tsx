7bf61638ac8839a7e5bf65888be0d811bfd6940a
Michael Shimeles <michaelwasihun96@gmail.com>
2025-06-02 15:47:12 -0400

    favicon

--- app/root.tsx
+++ app/root.tsx
@@ -30,6 +30,11 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: "icon",
+    type: "image/png",
+    href: "/favicon.png",
+  },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
